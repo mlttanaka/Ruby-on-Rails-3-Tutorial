6a876f7f7a5bd2678bbabbd8f8bb428f7bf65845
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-17 22:22:03 -0800

    Initial commit w/ Rspec and Capybara.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ruby on Rails Tutorial: sample application
+
+This is the sample application for
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
+
+Ruby 1.9.3
+Rails 3.2.14
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b448aae7cec2c83a2bbf8954790fc09317a2279991f955e81c4b84f4e57e3a35f81ce06ecec99b55bce31e86c3a05b1f51e771ebd5ea482d2f56d81fb1b61252'
