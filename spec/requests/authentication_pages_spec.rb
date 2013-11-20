d112519e3a465ce27f43b3e577b631f8b2d14056
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-20 15:42:47 -0800

    Test and implement sign out.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,6 +28,8 @@ describe "Authentication" do
   end
 
   describe "with valid information" do
+    before { visit signin_path }
+
     let(:user) { FactoryGirl.create(:user) }
     before do
       fill_in "Email",    with: user.email.upcase
@@ -39,5 +41,10 @@ describe "Authentication" do
     it { should have_link('Profile', href: user_path(user)) }
     it { should have_link('Sign out', href: signout_path) }
     it { should_not have_link('Sign in', href: signin_path) }
+
+    describe "followed by signout" do
+      before { click_link "Sign out" }
+      it { should have_link('Sign in') }
+    end
   end
 end
