b3d1fd5c2c2449a592574bb1e452ee1daec8e68c
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-22 18:47:19 -0800

    Test and implement destroy method for users and limit access to this method so that only admins can use it.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,14 @@
 FactoryGirl.define do
-  # Create factory sequences
+  # Create factory sequences for regular users.
   factory :user do
     sequence(:name)  { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+
+    # Create factory for admin user.
+    factory :admin do
+      admin true
+    end
   end
 end
